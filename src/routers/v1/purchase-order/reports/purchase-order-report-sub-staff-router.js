b1dbe40ff7ea2cb1d5b0c208f918be2e55a6ffc3
baguswidypriyono <kacagatot89@gmail.com>
2017-08-30 11:04:10 +0700

    Tambah Route
    Menambahkan Route untuk purchase repot staff dan report sub staff

---
+++ src/routers/v1/purchase-order/reports/purchase-order-report-sub-staff-router.js
@@ -0,0 +1,47 @@
+var Router = require('restify-router').Router;
+var db = require("../../../../db");
+var resultFormatter = require("../../../../result-formatter");
+const apiVersion = '1.0.0';
+var PurchaseOrderManager = require("dl-module").managers.purchasing.PurchaseOrderManager;
+var passport = require('../../../../passports/jwt-passport');
+
+function getRouter(){
+    var router = new Router();
+    router.get("/", passport, function(request, response, next) {
+        db.get().then(db => {
+            var manager = new PurchaseOrderManager(db, request.user);
+            var sdate = request.params.dateFrom;
+            var edate = request.params.dateTo;
+            var staff = request.params.staff;
+            var offset = request.headers["x-timezone-offset"] ? Number(request.headers["x-timezone-offset"]) : 0;
+
+            manager.getDataPODetailStaff(sdate, edate, staff, offset)
+                .then(docs => {
+                    if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
+                        var result = resultFormatter.ok(apiVersion, 200, docs);
+                        response.send(200, result);
+                    }
+                    else {
+                            
+                            var data = [];
+                            var index = 0;
+                            var selisihTotals=0;
+                            
+                            for (var Po of docs) {
+                                selisihTotals +=Po.selisih;
+                            }
+                            
+                    }
+                }).catch(e => {
+                    response.send(500, "gagal ambil data");
+                });
+
+        }).catch(e => {
+            var error = resultFormatter.fail(apiVersion, 400, e);
+            response.send(400, error);
+        });
+    });
+    return router;
+}
+
+module.exports = getRouter;
